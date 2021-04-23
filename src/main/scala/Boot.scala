84d8594b10950b3354d4fd423b933cbe4b7ab79e
hai <punchcleric85@gmail.com>
2021-04-23 16:50:35 +0300

    last endpoints: search, delete, update

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -7,15 +7,14 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-
-import com.haile.app.domain.User
+import com.haile.app.domain.{User, UserSearchParameters}
 import com.haile.app.dao.UserDAO
 import com.haile.app.failure.Failure
 import com.haile.app.util.DatabaseFailureMapper
 import com.haile.app.util.DateMarshalling._
 
 import scala.concurrent.Future
-import scala.util.{Success, Failure}
+import scala.util.{Failure, Success}
 
 
 
@@ -24,7 +23,8 @@ object Boot {
   implicit val system = ActorSystem(Behaviors.empty, "boot")
   implicit val executionContext = system.executionContext
 
-  implicit val userFormat = jsonFormat5(User)
+  implicit val userFormat       = jsonFormat5(User)
+  implicit val userParamFormat  = jsonFormat3(UserSearchParameters)
 
   val userDao = UserDAO
 
@@ -68,8 +68,64 @@ object Boot {
         },
 
         // Update User
+        put {
+          path("user" / LongNumber) {
+            id =>
+              entity(as[User]) {
+                user =>
+                  val changed: Future[Int] =
+                    userDao.update(id, user)
 
+                  onComplete(changed) {
+                    case Success(1) => complete("updated")
+                    case Success(_) => complete(
+                      DatabaseFailureMapper
+                        .notFoundError(id)
+                        .getStatusCode()
+                    )
+                    case scala.util.Failure(t) => failWith(t)
+                  }
+
+              }
+          }
+        },
+
+        // Delete User
+        delete {
+          path("user" / LongNumber) {
+            id =>
+              val deleted: Future[Int] =
+                userDao.delete(id)
+
+              onComplete(deleted) {
+                case Success(1) => complete("deleted")
+                case Success(_) => complete(
+                  DatabaseFailureMapper
+                    .notFoundError(id)
+                    .getStatusCode()
+                )
+                case scala.util.Failure(t) => failWith(t)
+              }
+          }
+        },
+
+        // Search User by Parameters
+        get {
+          path("user") {
+            entity(as[UserSearchParameters]) {
+              userSearch =>
+                val found: Future[Seq[User]] =
+                  userDao.search(userSearch)
+
+                onComplete(found) {
+                  case Success(users)         => complete(users)
+                  case scala.util.Failure(t)  => failWith(t)
+                }
+            }
+          }
+        }
       )
+
 
     // starting server
     val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)

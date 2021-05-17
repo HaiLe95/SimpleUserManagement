f19b73ebae64c931bd047912c61defd4a1126704
hai <punchcleric85@gmail.com>
2021-05-17 15:07:59 +0300

    fixed multiply route in main class

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -4,6 +4,7 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
 import controller.{BookController, UserController}
 
 object Boot {
@@ -13,10 +14,14 @@ object Boot {
     implicit val system = ActorSystem(Behaviors.empty, "boot")
     implicit val executionContext = system.executionContext
 
-    val route: Route = BookController.route
+    val bookRoute: Route = BookController.route
+    val userRoute: Route = UserController.route
+
+    val mainRoute: Route = concat(bookRoute, userRoute)
+
 
     // starting server
-    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(mainRoute)
 
   }
 
